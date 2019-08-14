ccf1f896ed91347d2232bf227685da2c21f4089d
Mario Johnson <johnson.mario8404@gmail.com>
2019-08-14 11:46:53 -0500

    Completed first section of Basic Statements Exercises

--- albums_migration.sql
+++ albums_migration.sql
@@ -2,10 +2,10 @@ USE codeup_test_db;
 DROP TABLE IF EXISTS albums;
 CREATE TABLE albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist_name VARCHAR(50),
-    record_name VARCHAR(50) NOT NULL,
+    artist_name VARCHAR(200),
+    record_name VARCHAR(200) NOT NULL,
     release_date INT,
     sales FLOAT,
-    genre CHAR(50),
+    genre CHAR(200),
     PRIMARY KEY (id)
 );
