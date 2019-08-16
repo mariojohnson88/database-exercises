b7c40854d8173a1a8b427718afb9af7b0ee74a64
Mario Johnson <johnson.mario8404@gmail.com>
2019-08-15 23:22:51 -0500

    Completed LIMIT Exercise

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -1,3 +1,14 @@
 USE employees;
 
-SELECT DISTINCT * FROM employees WHERE last_name like 'z%' limit 10;
+SELECT DISTINCT last_name FROM employees ORDER BY last_name DESC limit 10;
+
+SELECT first_name, last_name
+FROM employees
+WHERE hire_date like '199%' AND birth_date like '%12-25' ORDER BY birth_date, hire_date DESC
+LIMIT 5;
+
+SELECT first_name, last_name
+FROM employees
+WHERE hire_date like '199%' AND birth_date like '%12-25' ORDER BY birth_date, hire_date DESC
+LIMIT 5 OFFSET 45;
+
